d41096fe4f935b40494e4cf9abca6fa3c997ac30
Will Monroe <wmonroe4@stanford.edu>
2016-01-07 15:24:10 -0800

    Add squared error to evaluation metrics

--- bt/metrics.py
+++ bt/metrics.py
@@ -1,6 +1,15 @@
+import numpy as np
+
+
 def log_likelihood(eval_data, predictions, scores):
     return scores
 
+
 def accuracy(eval_data, predictions, scores):
     return [int(inst.output == pred)
+            for inst, pred in zip(eval_data, predictions)]
+
+
+def squared_error(eval_data, predictions, scores):
+    return [np.sum((np.array(pred) - np.array(inst.output)) ** 2)
             for inst, pred in zip(eval_data, predictions)]
