4ca02f265a8b2413a88cd861fb3a5ce7c38e8487
Will Monroe <wmonroe4@stanford.edu>
2016-02-11 20:49:42 -0800

    Add empty string debugging dataset

--- color_instances.py
+++ color_instances.py
@@ -53,6 +53,16 @@ def get_dev_instances(listener=False):
     return insts
 
 
+def empty_str(listener=False):
+    data = [('', (167.74193548404, 96.3730569948, 75.6862745098))]
+    return [
+        (Instance(input=name, output=color)
+         if listener else
+         Instance(input=color, output=name))
+        for name, color in data
+    ]
+
+
 def one_word(listener=False):
     data = [('green', (167.74193548404, 96.3730569948, 75.6862745098))]
     return [
@@ -68,4 +78,5 @@ DataSource = namedtuple('DataSource', ['train_data', 'test_data'])
 SOURCES = {
     'dev': DataSource(get_training_instances, get_dev_instances),
     '1word': DataSource(one_word, one_word),
+    '0word': DataSource(empty_str, empty_str),
 }
