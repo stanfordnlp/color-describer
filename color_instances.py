0bdf38f5b2f2faa20d81a2716e9e3f1bed7f29fb
Will Monroe <wmonroe4@stanford.edu>
2016-02-19 01:49:14 -0800

    Add scalar implicature toy dataset

--- color_instances.py
+++ color_instances.py
@@ -53,24 +53,42 @@ def get_dev_instances(listener=False):
     return insts
 
 
-def empty_str(listener=False):
-    data = [('', (167.74193548404, 96.3730569948, 75.6862745098))]
+def pairs_to_insts(data, listener=False):
     return [
         (Instance(input=name, output=color)
          if listener else
          Instance(input=color, output=name))
         for name, color in data
     ]
+
+
+def empty_str(listener=False):
+    data = [('', (167.74193548404, 96.3730569948, 75.6862745098))]
+    return pairs_to_insts(data, listener=listener)
 
 
 def one_word(listener=False):
     data = [('green', (167.74193548404, 96.3730569948, 75.6862745098))]
-    return [
-        (Instance(input=name, output=color)
-         if listener else
-         Instance(input=color, output=name))
-        for name, color in data
+    return pairs_to_insts(data, listener=listener)
+
+
+def scalar_imp_train(listener=False):
+    data = [
+        ('blue', (240., 100., 100.)),
+        ('blue', (180., 100., 100.)),
+        ('teal', (180., 100., 100.)),
     ]
+    return pairs_to_insts(data, listener=listener)
+
+
+def scalar_imp_test(listener=False):
+    data = [
+        ('blue', (240., 100., 100.)),
+        ('blue', (180., 100., 100.)),
+        ('teal', (240., 100., 100.)),
+        ('teal', (180., 100., 100.)),
+    ]
+    return pairs_to_insts(data, listener=listener)
 
 
 DataSource = namedtuple('DataSource', ['train_data', 'test_data'])
@@ -79,4 +97,5 @@ SOURCES = {
     'dev': DataSource(get_training_instances, get_dev_instances),
     '1word': DataSource(one_word, one_word),
     '0word': DataSource(empty_str, empty_str),
+    'scalar': DataSource(scalar_imp_train, scalar_imp_test),
 }
