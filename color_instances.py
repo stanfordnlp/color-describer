ad3fea4dc8e360f873dc9e8f2fcf7a2e5a644847
Will Monroe <wmonroe4@stanford.edu>
2016-06-02 14:27:19 -0700

    Add hsv-to-hsl conversion

--- color_instances.py
+++ color_instances.py
@@ -353,12 +353,18 @@ def hawkins_target(listener=False):
             for inst in insts]
 
 
+# HSL <-> HSV conversion based on C code by Ariya Hidayat:
+#   http://ariya.ofilabs.com/2008/07/converting-between-hsl-and-hsv.html
+
 def hsl_to_hsv(color):
     '''
-    >>> hsl_to_hsv((0, 100, 100))
-    (0.0, 0.0, 100.0)
     >>> hsl_to_hsv((120, 100, 50))
     (120.0, 100.0, 100.0)
+    >>> hsl_to_hsv((0, 100, 100))
+    (0.0, 0.0, 100.0)
+
+    Saturation in HSV is undefined and arbitrarily 0 for black:
+
     >>> hsl_to_hsv((240, 100, 0))
     (240.0, 0.0, 0.0)
     '''
@@ -367,9 +373,34 @@ def hsl_to_hsv(color):
     ho = hi
     si *= (li / 100.0) if li <= 50.0 else (1.0 - li / 100.0)
     vo = li + si
-    so = (200.0 * si / (li + si)) if vo else 0.0
+    so = (200.0 * si / vo) if vo else 0.0
 
     return (ho, so, vo)
+
+
+def hsv_to_hsl(color):
+    '''
+    >>> hsv_to_hsl((120, 100, 100))
+    (120.0, 100.0, 50.0)
+
+    Saturation in HSL is undefined and arbitrarily 0 for black and white:
+
+    >>> hsv_to_hsl((240, 0, 0))
+    (240.0, 0.0, 0.0)
+    >>> hsv_to_hsl((0, 0, 100))
+    (0.0, 0.0, 100.0)
+    '''
+    hi, si, vi = [float(d) for d in color]
+
+    ho = hi
+    lo = (200.0 - si) * vi / 200.0
+    so = si * vi / 200.0
+    if lo >= 100.0 or lo <= 0.0:
+        so = 0.0
+    else:
+        so /= (lo / 100.0) if lo <= 50.0 else (1.0 - lo / 100.0)
+
+    return (ho, so, lo)
 
 
 def uniform(color):
